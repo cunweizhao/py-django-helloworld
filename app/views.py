6a4f52465783d201cd81ae91150befcf0e62ed96
cunweizhao <18500103508@163.com>
2020-08-04 15:40:16 +0800

    添加音乐服务接口

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 
-from django.views.generic import View
-from django.http import HttpResponse
+# from django.views.generic import View
+# from django.http import HttpResponse
 #
 # class Index(View):
 #     def get(self,request,name,age):
@@ -22,9 +22,27 @@ from django.http import HttpResponse
 
 
 # 20200804
-class LessionTwo(View):
+# class LessionTwo(View):
+#
+#     def get(self,request):
+#
+#         message = request.GET.get('message','这里没有内容')
+#         return HttpResponse(message)
 
-    def get(self,request):
 
-        message = request.GET.get('message','这里没有内容')
-        return HttpResponse(message)
+# 获取音乐
+import requests
+from django.views.generic import View
+from django.http import JsonResponse
+
+class Music(View):
+
+      DOUBAN_MUSIC_API = 'https://api.douban.com/v2/music/search?q={0}'
+      def get(self,request):
+            music_name = request.GET.get('musicname','')
+            if not music_name:
+                return JsonResponse({'errcode':-1,'errmsg':'音乐名称不能为空'})
+            url = self.DOUBAN_MUSIC_API.format(music_name)
+            response = requests.get(url)
+            data = response.json()
+            return JsonResponse({'errcode':0,'errmsg':'成功',data:data})
