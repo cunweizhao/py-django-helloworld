fee7c7e96458afe0a03741be835a117a76ab2a44
cunweizhao <18500103508@163.com>
2020-08-04 14:37:53 +0800

    修改地区与时区

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -106,9 +106,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# 修改地区与时区
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'asia/Shanghai'
 
 USE_I18N = True
 
